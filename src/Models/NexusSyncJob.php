44843e0abeb026e798e03315beed097ce96ea3c2
Adnan Haider <deadpoolgob094@gmail.com>
2026-02-19 19:34:49 +0500

    fix: resolve PHPStan undefined property finished_at on NexusSyncJob

--- src/Models/NexusSyncJob.php
+++ src/Models/NexusSyncJob.php
@@ -5,6 +5,17 @@ namespace Adnan\LaravelNexus\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $type
+ * @property string|null $batch_id
+ * @property string $status
+ * @property array|null $meta
+ * @property \Illuminate\Support\Carbon|null $started_at
+ * @property \Illuminate\Support\Carbon|null $finished_at
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class NexusSyncJob extends Model
 {
     use HasFactory;
